f5744580cea81e68580552ac7761c875b2545376
Luidi3159 <luidi596o@gmail.com>
2026-07-01 13:01:13 -0300

    animacao personagem pronto

--- Assets/Scripts/MovimentaçãoPlayer.cs
+++ Assets/Scripts/MovimentaçãoPlayer.cs
@@ -4,17 +4,20 @@ using UnityEngine.InputSystem;
 
 public class MovimentaçãoPlayer : MonoBehaviour
 {
-    
     private CharacterController ch;
+    private Animator anim;
+    [SerializeField] private Transform modelo;
     private float movementX;
     private float movementY;
     public float speed = 5;
+    public float rotationSpeed = 10f;
     private float tempSpeed;
-
+    
     void Start()
     {
         tempSpeed = speed;
         ch = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
@@ -34,12 +37,31 @@ public class MovimentaçãoPlayer : MonoBehaviour
         {
             tempSpeed = speed;
         }
+
     }
 
     void Update()
     {
+        
+
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         Debug.Log(movement);
+        if (movement != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+
+            modelo.rotation = Quaternion.Slerp(
+                modelo.rotation,
+                targetRotation,
+                rotationSpeed * Time.deltaTime
+            );
+        }
         ch.Move(movement * tempSpeed * Time.deltaTime);
+        float valorSpeed = movement.magnitude * tempSpeed;
+
+        Debug.Log(valorSpeed);
+
+        anim.SetFloat("Speed", valorSpeed);
+        anim.SetFloat("Speed", movement.magnitude * tempSpeed);
     }
 }
